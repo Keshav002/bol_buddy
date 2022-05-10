e4390ec02c9d981cdb0406a22edbb61789e18fcb
Keshav <keshavtayal002@gmail.com>
2022-05-10 21:02:26 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from './components/Nav'
+import Contact from './components/Contact'
+// import About from './components/About'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Videos from './components/Videos'
+import Choice from './components/Choice'
+import Readings from './components/Readings'
+// import Logout from './components/Logout'
+import './App.css'
+import { initialState, reducer } from './reducer/UseReducer';
 
-function App() {
+//context api
+export const UserContext = createContext();
+
+const Routing = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Router>
+    <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Routes>
+        <Route path="/videos" element={<Videos />} />
+      </Routes>
+      <Routes>
+        <Route path="/readings" element={<Readings />} />
+      </Routes>
+      <Routes>
+        <Route path="/choice" element={<Choice />} />
+      </Routes>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+
+    <>
+      <UserContext.Provider value={{ state, dispatch }}>
+        <Routing />
+      </UserContext.Provider>
+    </>
+  )
+}
+
+export default App
